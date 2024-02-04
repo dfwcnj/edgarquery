29196a0d028c0e838538ad13eae727d40f198ed9
don caldwell <doncaldwell@dfwcMBM1.local>
2024-02-04 10:59:00 -0500

    change organization

--- src/edgarquery/__init__.py
+++ src/edgarquery/__init__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2023-present U.N. Owen <void@some.where>
+# SPDX-FileCopyrightText: 2023-present Don Caldwell <dfwcnj@gmail.com>
 #
 # SPDX-License-Identifier: MIT
 # https://www.sec.gov/edgar/sec-api-documentation
